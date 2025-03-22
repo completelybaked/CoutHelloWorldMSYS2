5b78d4e4d4d8a96e23c89bee43b0181036cd9eea
completelybaked <jwelke@sbcglobal.net>
2025-03-22 11:06:32 -0400

    Add files via upload
    Initial commit of hello world using std:cout

---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream> // Include the iostream header
+
+int main() {
+    std::cout << "Yabba, dabba, doo! Hello, World!" << std::endl; // Use std::cout to print to the console
+    return 0;
+}
